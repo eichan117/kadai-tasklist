145a0b86e7dd0482b9d1f148a941daea04b0e015
eichan <koro37256@gmail.com>
2020-05-11 10:31:02 +0000

    kadai-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
  before_action :set_task,only: [:show, :edit, :update, :destroy]
- 
+ before_action :require_user_logged_in
+ before_action :correct_user, only: [:destroy]
  def index
-  @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end 
  end
 
   def show
@@ -13,15 +17,15 @@ class TasksController < ApplicationController
   end
 
   def create
-   @task = Task.new(task_params)
-   
-   if @task.save
-    flash[:success] = 'Taskが正常に投稿されました'
-    redirect_to @task
-   else
-    flash.now[:danger] = 'Taskが投稿されませんでした'
-    render :new
-   end
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'tasks/index'
+    end  
   end
 
   def edit
@@ -40,13 +44,12 @@ class TasksController < ApplicationController
 
   def destroy
    @task.destroy
-   
-   flash[:success] = 'Taskは正常に削除されました'
-   redirect_to tasks_url
+   flash[:success] = 'Taskを削除しました。'
+   redirect_back(fallback_location: root_path)
   end
 
 
-  private
+private
   def set_task
    @task = Task.find(params[:id])
   end
@@ -54,5 +57,11 @@ class TasksController < ApplicationController
   def task_params
    params.require(:task).permit(:content,:status)
   end 
-
-end
+  
+  def correct_user
+   @task = current_user.tasks.find_by(id: params[:id])
+   unless @task
+    redirect_to root_url
+   end
+end   
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'newtasks/create'
+  get 'newtasks/destroy'
     root to: 'tasks#index'
-    
     resources :tasks
     
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
+    get 'signup',to: 'users#new'
+    resources :users, only: [:index, :show, :create]
+    
+    resources :tasks,only: [:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
